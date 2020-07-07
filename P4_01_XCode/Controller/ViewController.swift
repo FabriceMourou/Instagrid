07c5f3a20fc4dcfbab200b8f37c1445df5e1cdb3
FabriceMourou <shiimejiii@gmail.com>
2020-07-07 09:40:30 +0200

    Updated version  07/07/20

--- P4_01_XCode/Controller/ViewController.swift
+++ P4_01_XCode/Controller/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var gridView: UIView!
     @IBOutlet weak var topStackView: UIStackView!
     @IBOutlet weak var botStackView: UIStackView!
     
@@ -19,7 +20,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var arrowLeft: UIButton!
     
     
-  
+    @IBAction func didSwipeToShare(_ sender: UISwipeGestureRecognizer) {
+        sharePhotoLayout()
+    }
     
     private let photoLayoutProvider = PhotoLayoutProvider()
     
@@ -27,6 +30,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupPhotoLayoutButtons()
+        
+
+    }
+    
+    private func renderGridViewAsImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: gridView.bounds)
+        return renderer.image { rendererContext in
+            gridView.layer.render(in: rendererContext.cgContext)
+        }
+    }
+    
+    private func sharePhotoLayout() {
+        UIView.animate(withDuration: 0.3) {
+            self.gridView.transform = CGAffineTransform(translationX: 0, y: -self.view.frame.height)
+        }
+        
+        let image = renderGridViewAsImage()
+
+        let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        activityViewController.completionWithItemsHandler = { _, _, _, _ in
+            UIView.animate(withDuration: 0.3) {
+                self.gridView.transform = .identity
+            }
+        }
+        
+        present(activityViewController, animated: true)
+    }
+    
+    private func setupPhotoLayoutButtons() {
         for (index, _) in photoLayoutProvider.layouts.enumerated() {
             let button = UIButton()
             let buttonImage = UIImage(named: "Layout \(index + 1)")
@@ -35,7 +69,6 @@ class ViewController: UIViewController {
             button.addTarget(self, action: #selector(setupLayout(sender:)), for: .touchUpInside)
             layoutButtonsStackView.addArrangedSubview(button)
         }
-    
     }
     
     @objc private func setupLayout(sender: UIButton) {
@@ -57,23 +90,53 @@ class ViewController: UIViewController {
     
     
     private func setupStackViewsFromLayout(photoLayout: PhotoLayout) {
-        addMultipleWhiteViewToStackView(amount: photoLayout.numberOfPhotosTop, stackView: topStackView)
-        addMultipleWhiteViewToStackView(amount: photoLayout.numberOfPhotosBot, stackView: botStackView)
+        addMultiplePhotoButtonsToStackView(amount: photoLayout.numberOfPhotosTop, stackView: topStackView)
+        addMultiplePhotoButtonsToStackView(amount: photoLayout.numberOfPhotosBot, stackView: botStackView)
     }
     
-    private func addMultipleWhiteViewToStackView(amount: Int, stackView: UIStackView) {
+    private func addMultiplePhotoButtonsToStackView(amount: Int, stackView: UIStackView) {
         for _ in 1...amount {
-            addWhiteViewToStackView(stackView: stackView)
+            addPhotoButtonToStackView(stackView: stackView)
         }
+        
     }
     
-    private func addWhiteViewToStackView(stackView: UIStackView) {
-        let whiteView = UIView()
-        whiteView.backgroundColor = .white
-        stackView.addArrangedSubview(whiteView)
+    private func addPhotoButtonToStackView(stackView: UIStackView) {
+        let plusButton = UIButton()
+        plusButton.backgroundColor = .white
+        plusButton.imageView?.contentMode = .scaleAspectFill
+        let plusImage = UIImage(named: "Plus")
+        plusButton.setImage(plusImage, for: .normal)
+        plusButton.addTarget(self, action: #selector(displayImagePickerController(sender:)), for: .touchUpInside)
+        stackView.addArrangedSubview(plusButton)
+    }
+    
+    var selectedButton: UIButton?
+    
+    
+    @objc private func displayImagePickerController(sender: UIButton) {
+        
+        selectedButton = sender
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true)
+        
+        
+        print ("will display imagePickerController")
     }
 
+}
+
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        selectedButton?.setImage(selectedImage, for: .normal)
+        
+        dismiss(animated: true, completion: nil)
+    }
     
 }
 
