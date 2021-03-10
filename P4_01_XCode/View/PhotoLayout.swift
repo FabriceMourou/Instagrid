e64b2a7e94693d84bf2846578bd8dd83045d292d
FabriceMourou <fabrice.mourou@icloud.com>
2021-03-10 16:34:33 +0100

    Revert "Develop"

--- P4_01_XCode/View/PhotoLayout.swift
+++ P4_01_XCode/View/PhotoLayout.swift
@@ -10,16 +10,6 @@ import UIKit
 
 
 struct PhotoLayout {
-    
-    // MARK: - Internal
-    
-    // MARK: Properties - Internal
-    
     let numberOfPhotosTop: Int
     let numberOfPhotosBot: Int
-    
-    
-    var totalNumberOfPhotos: Int {
-        numberOfPhotosTop + numberOfPhotosBot
-    }
 }
